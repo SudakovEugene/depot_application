18de2e423af396e4423bcc67ea21223fa5e0c99c
SudakovEugene <SudakovEugene@gmail.com>
2016-08-22 17:36:28 +0300

    Add RSS channel Atom for check orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root to: 'store#index', as: 'store'
-
   get 'store/index'
 
   resources :orders
   resources :line_items
   resources :carts
   resources :products
+
+  resources :products do
+    get :who_bought, on: :member
+  end
 
 end
