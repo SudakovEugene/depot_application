1022bf24f80457924dc998b5a7953192883965b1
SudakovEugene <SudakovEugene@gmail.com>
2016-08-22 16:48:12 +0300

    Add possibility to make order for items in the cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :line_items
-  resources :carts
+  root to: 'store#index', as: 'store'
+
   get 'store/index'
 
+  resources :orders
+  resources :line_items
+  resources :carts
   resources :products
 
-  root to: 'store#index', as: 'store'
 end
